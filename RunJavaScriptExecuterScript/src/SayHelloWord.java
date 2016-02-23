ea0cf246fe0b1faaafab0609ea6b9a5abb2aa971
mandar100 <mandar_tankasale@persistent.com>
2016-02-23 14:04:20 +0530

    checking simple working project

---
+++ RunJavaScriptExecuterScript/src/SayHelloWord.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mandar_Tankasale
+ *
+ */
+public class SayHelloWord {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow to git hub and eclipse integration");
+	}
+
+}
